4df88c7b81cd295d11eb3b6775b00da641bc7069
Derek Lindahl <derek.lindahl@gmail.com>
2017-04-10 14:19:17 -0700

    Add initial JIRA Webhook parsing

--- jira-worklog-webhook/handler.js
+++ jira-worklog-webhook/handler.js
@@ -1,11 +1,30 @@
 'use strict';
 
 module.exports.worklogWebhook = (event, context, callback) => {
+  const user = event.queryStringParameters.user_key;
+  const issueKey = event.queryStringParameters.issueKey;
+  const projectId = event.queryStringParameters.projectId;
+
+  const body = JSON.parse(event.body);
+  const comment = body.worklog.comment;
+  const timeSpentSeconds = body.worklog.timeSpentSeconds;
+  const webhookEvent = body.webhookEvent;
+
+  console.info({
+    webhookEvent,
+    user,
+    issueKey,
+    projectId,
+    timeSpentSeconds,
+    comment,
+    user,
+  });
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
+      message: 'Go Serverless v1.0! Your function executed successfully!',
     }),
   };
 
