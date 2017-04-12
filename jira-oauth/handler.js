e31da853fa91a69a166d137f8cbb96d1cd052867
Derek Lindahl <derek.lindahl@gmail.com>
2017-04-12 13:39:50 -0700

    Add reusable Promise responders to jira-oauth handler file

--- jira-oauth/handler.js
+++ jira-oauth/handler.js
@@ -1,14 +1,23 @@
 const requestOauthToken = require('./requestOauthToken');
 
+function fail(callback) {
+  return error => {
+    if (error.isRedirect) {
+      throw error;
+    }
+    console.error(error);
+    return callback(error);
+  };
+}
+
+function ok(callback) {
+  return response => {
+    return callback(null, response);
+  };
+}
+
 // Initiates the OAuth dance by requesting a token from JIRA
 module.exports.requestOauthToken = (event, context, callback) => {
-  return requestOauthToken(event)
-    .then(response => callback(null, response))
-    .catch(error => {
-      if (error.isRedirect) {
-        throw error;
-      }
-      console.error(error);
-      return callback(error);
-    });
+  return requestOauthToken(event).then(ok(callback)).catch(fail(callback));
+};
 };
